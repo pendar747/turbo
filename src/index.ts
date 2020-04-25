22af65f8fa0b6bec32b41febd067b7d9ba4165da
Pendar <pendar747@gmail.com>
2020-04-25 19:27:14 +0100

    update readme

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import If from './components/If';
+import Render from './components/Render';
+import TurboComponent from './components/TurboComponent';
+import Unless from './components/Unless';
+import State from './state/State';
+
+export {
+  If,
+  Render,
+  TurboComponent,
+  Unless,
+  State
+}
