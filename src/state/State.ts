975ba0b3ab82c486c766e0293beb8d03ba6a3067
Pendar <pendar747@gmail.com>
2020-05-04 00:28:27 +0100

    State with basic test

--- src/state/State.ts
+++ src/state/State.ts
@@ -1,6 +1,4 @@
 import { fire, on } from "../util";
-import get from "lodash/get";
-import fromPairs from 'lodash/fromPairs';
 
 export default class State<StateT, ComputedT> {
 
@@ -8,30 +6,25 @@ export default class State<StateT, ComputedT> {
 
   protected name: string;
 
-  get allState ():  StateT & { computed :ComputedT } {
-    const computed: ComputedT = fromPairs(this._computedProperties
-      .map(propertyName => [propertyName, get(this, propertyName)])) as ComputedT;
-    return { ...this._state, computed };
-  }
-
   constructor(name: string, initialState: StateT, protected _computedProperties: string[] = []) {
     this.name = name;
     this._state = initialState;
     this.update(initialState);
   }
 
-  update (newSate: StateT) {
+  public onUpdated (callback: (newState: StateT) => void) {
+    on(`new-state:${this.name}`, (event) => {
+      callback(event.detail.state);    
+    });
+  }
+
+  public update (newSate: StateT) {
     this._state = newSate;
     this.saveStateToSession();
-    console.log(`${this.name}-state-update`.toUpperCase(), { state: this.allState })
-    fire(`${this.name}-state-update`, { state: this.allState });
+    fire(`new-state:${this.name}`, { state: this._state });
   }
 
   private saveStateToSession () {
-    window.sessionStorage.setItem(`${this.name}-state`, JSON.stringify(this.allState));
-  }
- 
-  get current (): StateT {
-    return { ...this._state };
+    window.sessionStorage.setItem(`${this.name}-state`, JSON.stringify(this._state));
   }
 }
