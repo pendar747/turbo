e57cbde7878acb81665a26d237a695366fb312fa
Pendar <pendar747@gmail.com>
2020-05-06 01:19:22 +0100

    remove state

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,10 @@ import If from './components/If';
 import Render from './components/Render';
 import TurboComponent from './components/TurboComponent';
 import Unless from './components/Unless';
-import State from './state/State';
 
 export {
   If,
   Render,
   TurboComponent,
-  Unless,
-  State
+  Unless
 }
