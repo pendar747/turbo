bad38e38f8945d1d1746d353f085797e9fb07c66
Pendar <pendar747@gmail.com>
2020-06-20 16:30:54 +0100

    refactor getRequestedState

--- src/modelAdapters/mobX/registerState.ts
+++ src/modelAdapters/mobX/registerState.ts
@@ -1,25 +1,8 @@
 import { autorun, observable } from 'mobx';
 
 import get from 'lodash-es/get';
-import set from 'lodash-es/set';
-import mergeWith from 'lodash-es/mergeWith';
 import { MessageData } from './types';
-
-const serialize = (obj: any) => JSON.parse(JSON.stringify(obj));
-
-const mergeCustomizer = (obj1: any, obj2: any) => {
-  if(Array.isArray(obj1) && Array.isArray(obj2) && obj1.length !== obj2.length) {
-    const largerArray = obj1.length > obj2.length ? obj1 : obj2;
-    const smallerArray = obj1.length > obj2.length ? obj2 : obj1;
-    largerArray.map((item, index) => {
-      if (smallerArray[index]) {
-        return mergeWith(item, smallerArray[index], mergeCustomizer);
-      }
-      return item;
-    });
-  }
-  return mergeWith(obj1, obj2);
-}
+import getRequestedState from '../getRequestedState';
 
 const registerState = (stateName: string) => (StateClass: any) => {
   const state = new StateClass();
@@ -27,23 +10,10 @@ const registerState = (stateName: string) => (StateClass: any) => {
   const getters: any = observable.box([]);
   
   autorun(() => {
-    let data = getters.get().length ? {} : state;
-    // todo move this to a separate module and thoroughly test it
-    getters.get().forEach((property: string) => {
-      const value = get(state, property);
-      const existingValue = get(data, property);
-      if (typeof existingValue == 'object' && typeof value == 'object') {
-        set(data, property, mergeWith(existingValue, serialize(value), mergeCustomizer));
-      } else if(typeof value == 'object') {
-        set(data, property, serialize(value));
-      } else {
-        set(data, property, value);
-      }
-    })
     postMessage({
       type: 'state-update',
       stateName,
-      data: serialize(data)
+      data: getRequestedState(getters.get(), state)
     } as MessageData);
   });
 
