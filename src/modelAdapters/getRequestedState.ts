1ddf0fe483d13147dfdf7947e422487e319839ff
Pendar <pendar747@gmail.com>
2020-06-20 23:00:23 +0100

    fix issue with undefined values getting merged

--- src/modelAdapters/getRequestedState.ts
+++ src/modelAdapters/getRequestedState.ts
@@ -1,17 +1,22 @@
 import get from 'lodash-es/get';
 import set from 'lodash-es/set';
 import merge from 'lodash-es/merge';
+import isNil from 'lodash-es/isNil';
 
 const serialize = (obj: any) => JSON.parse(JSON.stringify(obj));
 
 const getRequestedState = (getters: string[], state: { [key in string]: any }) => {
   let data = getters.length ? {} : state;
   getters.forEach((property: string) => {
-    const newData = {};
     const value = get(state, property);
     const serializedValue = typeof value === 'object' ? serialize(value) : value;
-    set(newData, property, serializedValue);
-    data = merge(data, newData);
+    if (isNil(value)) {
+      set(data, property, value);
+    } else {
+      const newData = {};
+      set(newData, property, serializedValue);
+      data = merge(data, newData);
+    }
   })
 
   return serialize(data);
