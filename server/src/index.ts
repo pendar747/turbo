4ce4260ac85ed9849d310519bd60074d56f6bb3b
Pendar <pendar747@gmail.com>
2020-07-22 14:46:39 +0100

    basic build

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,38 +2,41 @@ import path from 'path';
 import fs from 'fs';
 import express from 'express';
 
-const readTemplates = async (templatesPath: string) => {
-  const dir = await fs.promises.opendir(templatesPath);
-  for await (const dirent of dir) {
-    if (dirent.isDirectory()) {
-      console.log('foo');
-    }
-  }
-}
+// const readTemplates = async (templatesPath: string) => {
+//   const dir = await fs.promises.opendir(templatesPath);
+//   for await (const dirent of dir) {
+//     if (dirent.isDirectory()) {
+//       console.log('foo');
+//     }
+//   }
+// }
 
-export const createMiddleware = (templatesPath: string, statePath: string) => {
-  const templateTree = readTemplates(templatesPath);
-  const State = buildState(statePath);
+// export const createMiddleware = (templatesPath: string, statePath: string) => {
+//   const templateTree = readTemplates(templatesPath);
+//   const State = buildState(statePath);
 
-  const middleware = (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    const parameters = getParameters(req);
-    const state = runState(State, parameters);
-    if (routeExists(templateTree)) {
-      const htmlResponse = buildHtmlResponse(templateTree, state, parameters);
-      res.send(htmlResponse);
-    } else {
-      next();
-    }
-  };
+//   const middleware = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+//     if (req.path.indexOf('/template') === 0) {
+//       return serveTemplates(req, res, next);
+//     }
+//     const parameters = getParameters(req);
+//     const state = runState(State, parameters);
+//     if (routeExists(templateTree)) {
+//       const htmlResponse = buildHtmlResponse(templateTree, state, parameters);
+//       res.send(htmlResponse);
+//     } else {
+//       next();
+//     }
+//   };
 
-  return middleware;
-}
+//   return middleware;
+// }
 
 
 const server = express();
 
-const middleware = createMiddleware('/templates', '/state');
-server.use(middleware);
+// const middleware = createMiddleware('/templates', '/state');
+// server.use(middleware);
 
 server.listen(5000, () => {
   console.log('Listening on port 5000');
