4bf61d60bab14dc4d10beb24a721942131b399ad
Pendar <pendar747@gmail.com>
2020-07-22 14:24:01 +0100

    server package

---
+++ server/src/index.ts
@@ -0,0 +1,40 @@
+import path from 'path';
+import fs from 'fs';
+import express from 'express';
+
+const readTemplates = async (templatesPath: string) => {
+  const dir = await fs.promises.opendir(templatesPath);
+  for await (const dirent of dir) {
+    if (dirent.isDirectory()) {
+      console.log('foo');
+    }
+  }
+}
+
+export const createMiddleware = (templatesPath: string, statePath: string) => {
+  const templateTree = readTemplates(templatesPath);
+  const State = buildState(statePath);
+
+  const middleware = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const parameters = getParameters(req);
+    const state = runState(State, parameters);
+    if (routeExists(templateTree)) {
+      const htmlResponse = buildHtmlResponse(templateTree, state, parameters);
+      res.send(htmlResponse);
+    } else {
+      next();
+    }
+  };
+
+  return middleware;
+}
+
+
+const server = express();
+
+const middleware = createMiddleware('/templates', '/state');
+server.use(middleware);
+
+server.listen(5000, () => {
+  console.log('Listening on port 5000');
+});
